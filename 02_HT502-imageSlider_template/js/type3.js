1bf4e94443c4390a23ba46c734b26f0e20201a96
Yang Hyunil <xguest7@naver.com>
2018-12-02 15:31:16 +0900

    type4 완성

--- 02_HT502-imageSlider_template/js/type3.js
+++ 02_HT502-imageSlider_template/js/type3.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
     $("#slider ul li.item:gt(0)").hide();
 
     var fading;
-
+    
     function start() {
         fading=setInterval(function(){
             $("#slider ul li.item:first").fadeOut(1000).next().fadeIn(1000).end().appendTo("#slider ul");
